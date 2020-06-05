867cf5638d80a4c000ef4c8fd0420c24144ae4c8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-05 15:59:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 2 exercice 1</title>
+</head>
+<body>
+    <p>
+        <?php
+            $age = 24;
+            if ($age >= 18) {
+                echo 'vous êtes majeur';
+            }else {
+                echo 'vous êtes mineur';
+            }
+
+        ?>
+    </p>
+
+</body>
+</html>
